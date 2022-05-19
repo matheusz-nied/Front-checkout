4e82a0d2fd0c3453ddb4795f1ad99798e1206bb0
Sehmer Desktop <matheustae@hotmail.com>
2022-05-19 15:49:17 -0300

    Fineshed

--- js/listagem.js
+++ js/listagem.js
@@ -11,64 +11,64 @@ function listAllOfMonth() {
 
         `
     );
-    // const results = fetch(
-    //     "https://portifolio-nied-checkout.herokuapp.com/api/checkoutDay/listAll"
-    // )
-    //     .then((response) => response.json())
-    //     .then((response) => {
-        
+    const results = fetch(
+        "https://portifolio-nied-checkout.herokuapp.com/api/checkoutDay/listAll"
+    )
+        .then((response) => response.json())
+        .then((response) => {
+
             
 
-    //         containerListagem.innerHTML = "";
-    //         response.sort((checkout, otherCheckout) => {
-    //             return checkout.day < otherCheckout.day
-    //                 ? 1
-    //                 : checkout.day > otherCheckout.day
-    //                 ? -1
-    //                 : 0;
-    //         });
+            containerListagem.innerHTML = "";
+            response.sort((checkout, otherCheckout) => {
+                return checkout.day < otherCheckout.day
+                    ? 1
+                    : checkout.day > otherCheckout.day
+                    ? -1
+                    : 0;
+            });
 
-    //         if (response.length > 0) {
-    //             let sale_of_month = 0;
+            if (response.length > 0) {
+                let sale_of_month = 0;
 
-    //             response.forEach((checkout) => {
-    //                 sale_of_month += checkout.sale_day;
-    //             });
+                response.forEach((checkout) => {
+                    sale_of_month += checkout.sale_day;
+                });
 
-    //             let sale_of_month_field =
-    //                 document.getElementById("#sale-of-month");
-    //             sale_of_month_field.innerHTML = Number(sale_of_month);
-    //         }
+                let sale_of_month_field =
+                    document.getElementById("#sale-of-month");
+                sale_of_month_field.innerHTML = Number(sale_of_month);
+            }
 
-    //         response.map((data) => {
-    //             let date = new Date(data.day);
-    //             let dayFormated =
-    //                 addZero(date.getDate().toString()) +
-    //                 "/" +
-    //                 addZero(date.getMonth() + 1).toString() +
-    //                 "/" +
-    //                 date.getFullYear();
+            response.map((data) => {
+                let date = new Date(data.day);
+                let dayFormated =
+                    addZero(date.getDate().toString()) +
+                    "/" +
+                    addZero(date.getMonth() + 1).toString() +
+                    "/" +
+                    date.getFullYear();
 
-    //             containerListagem.insertAdjacentHTML(
-    //                 "beforeend",
-    //                 `
-    //                 <a onClick="saveDataSessionStorage(this)" href="/edit.html">
-    //                 <div class="nied-card">
-    //                     <div class="uk-card uk-card-primary uk-card-hover uk-card-body uk-light nied-card-body">
-    //                         <h3>${dayFormated}</h3>                         
+                containerListagem.insertAdjacentHTML(
+                    "beforeend",
+                    `
+                    <a onClick="saveDataSessionStorage(this)" href="/edit.html">
+                    <div class="nied-card">
+                        <div class="uk-card uk-card-primary uk-card-hover uk-card-body uk-light nied-card-body">
+                            <h3>${dayFormated}</h3>                         
                             
-    //                         <p >Venda do dia: <span>R$ ${data.sale_day}</span></p>
+                            <p >Venda do dia: <span>R$ ${data.sale_day}</span></p>
 
-    //                         <input type="hidden" id=#${data.id} value=${data.id} />
+                            <input type="hidden" id=#${data.id} value=${data.id} />
                            
 
 
-    //                     </div>
-    //                 </div>
-    //                 </a>`
-    //             );
-    //         });
-    //     });
+                        </div>
+                    </div>
+                    </a>`
+                );
+            });
+        });
 }
 
 function addZero(numero) {
